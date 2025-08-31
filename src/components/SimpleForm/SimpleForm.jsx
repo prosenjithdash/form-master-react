921018d05151a9b8c680c2fc8c5e8256b51a9bda
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-09-01 01:38:43 +0600

    Completed Handle form with onSubmit and access field data

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const SimpleForm = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Form Submitted.')
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+        console.log(e.target.password.value)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name' placeholder='Type name' />
+                <br />
+                <input type="email" name='email' placeholder='Type email' />
+                <br />
+                <input type="password" name='password' placeholder='Type password' />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
