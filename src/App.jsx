357e2dc422ad1aee7395c22f1d84ca21f33f1c68
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-09-03 06:32:59 +0600

    practice done (advanced) Reusable form using props and children

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 
 import './App.css'
 import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
 // import RefForm from './components/RefForm/RefForm'
 import SimpleForm from './components/SimpleForm/SimpleForm'
 import StateFulForm from './components/StateFulForm/StateFulForm'
 
 function App() {
 
+  // const handleSignUpSubmit = (data) => {
+  //   console.log('SignUp data: ',data)
+
+  // }
+
+  // const handleUpdateProfile = (data) => {
+  //   console.log('Update Profile data: ', data)
+
+  // }
+
+  const handleSignup = (data) => {
+    console.log('Signup data:',data)
+  }
+  const handleUpdate = (data) => {
+    console.log('Update data:',data)
+  }
   return (
     <div>
       <h1>Form Master React</h1>
       {/* <SimpleForm/> */}
       {/* <StateFulForm/> */}
       {/* <RefForm/> */}
-      <HookForm/>
+      {/* <HookForm/> */}
+      {/* <ReusableForm formTitle={'Sign Up'} handleSubmit={handleSignUpSubmit} />
+      <ReusableForm formTitle={'Profile Update '} submitBtnText='Update' handleSubmit={handleUpdateProfile}/> */}
+
+
+
+      <ReusableForm fromTitle='SignUp' handleSubmit={handleSignup} />
+      <ReusableForm fromTitle='User Update Profile' submitBTNText='Update' handleSubmit={handleUpdate}/>
+
     </div>
   )
 }
---
+++ src/components/ReusableForm/ReusableForm.jsx
@@ -0,0 +1,66 @@
+// import React from 'react';
+
+// const ReusableForm = ({formTitle , handleSubmit, submitBtnText='Submit'}) => {
+
+//     const handleLocalSubmit = (e) => {
+//         e.preventDefault();
+//         const data = {
+//             name: e.target.name.value,
+//             email: e.target.email.value,
+//             password: e.target.password.value
+
+
+//         }
+//         handleSubmit(data)
+
+//     }
+//     return (
+//         <div>
+//             <h2>{formTitle}</h2>
+//             <form onSubmit={handleLocalSubmit}>
+//                 <input type="text" name='name' placeholder='Type name' />
+//                 <br />
+//                 <input type="email" name='email' placeholder='Type email' />
+//                 <br />
+//                 <input type="password" name='password' placeholder='Type password' />
+//                 <br />
+//                 <input type="submit" value={submitBtnText} />
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default ReusableForm;
+
+
+import React from 'react';
+
+const ReusableForm = ({ fromTitle,handleSubmit, submitBTNText='Submit'}) => {
+
+    const handleLocalSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+        handleSubmit(data)
+
+    }
+    return (
+        <div>
+            <h2>{ fromTitle}</h2>
+            <form onSubmit={handleLocalSubmit}>
+                <input type="text" name='name' placeholder='Type name' />
+                <br />
+                <input type="email" name='email' placeholder='Type email' />
+                <br />
+                <input type="password" name='password' placeholder='Type password' />
+                <br />
+                <input type="submit" value={submitBTNText} />
+            </form>
+        </div>
+    );
+};
+
+export default ReusableForm;
